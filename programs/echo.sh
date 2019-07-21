83f249fc5cb3bcfd3d779a403b5917f942dd71cb
rahul <rp@gmail.com>
2019-07-21 12:41:27 +1000

    added new program echo

---
+++ programs/echo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+################################################
+# This program is a simple dislay of message 
+# Taking value into a variable from a command  
+# Taking values into a variable 
+###############################################
+
+
+datetime=`date`
+name="pallavi"
+echo "Todays date and time is : ${datetime} "
+echo "Name is $name"
+
+
